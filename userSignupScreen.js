e982088b2f4211f8760100dcfbf9bdaddc113abb
shota-imoto <hideyoshi.playing.the.banjo@gmail.com>
2021-01-18 22:20:51 +0900

    テキストフォームの追加、テキストフォーム入力機能の実装完了

--- userSignupScreen.js
+++ userSignupScreen.js
@@ -1,12 +1,94 @@
 import React from 'react';
-import { SafeAreaView, View, FlatList, StyleSheet, Text, StatusBar } from 'react-native';
+import { SafeAreaView, View, FlatList, StyleSheet, Text, TextInput } from 'react-native';
 
-function UserSignupScreen() {
+class Forms extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      password: '',
+      password_confirmation: ''
+    }
+    this.handleChangeEmail = this.handleChangeEmail.bind(this)
+    this.handleChangePassword = this.handleChangePassword.bind(this)
+  }
+
+  handleChangeEmail(text) {
+    this.setState({
+      email: text
+    });
+  };
+
+  handleChangePassword(text) {
+    this.setState({
+      password: text
+    });
+  }
+
+  handleChangePasswordConfirmation(text) {
+    this.setState({
+      password_confirmation: text
+    });
+  }
+
+  render() {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>User Signup Screen</Text>
+    <View>
+      <TextInput
+        style={ styles.textForm }
+        value={ this.state.email }
+        onChangeText={ text => this.handleChangeEmail(text) }
+        autoCapitalize={ 'none' }
+        autoCompleteType={ 'email' }
+        textContentType={ 'emailAddress' }
+      />
+      <TextInput
+        style={ styles.textForm }
+        value={ this.state.password }
+        onChangeText={ text => this.handleChangePassword(text) }
+        autoCapitalize={ 'none' }
+        autoCompleteType={ 'password' }
+        secureTextEntry={ true }
+        textContentType={ 'password' }
+        clearTextOnFocus={ false }
+
+      />
+      <TextInput
+        style={ styles.textForm }
+        value={ this.state.password_confirmation }
+        onChangeText={ text => this.handleChangePasswordConfirmation(text) }
+        autoCapitalize={ 'none' }
+        autoCompleteType={ 'password' }
+        secureTextEntry={ true }
+        textContentType={ 'password' }
+      />
     </View>
-  );
+  )
+  }
 }
+
+class UserSignupScreen extends React.Component {
+  constructor(props) {
+    super(props);
+  };
+
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>User Signup Screen</Text>
+        <Forms />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  textForm: {
+    height: 30,
+    width: 200,
+    borderColor: 'gray',
+    borderWidth: 1
+  }
+})
 
 export default UserSignupScreen;
