46b0274c1142d0de038f6a0deff6d452dbc1e0c1
shota-imoto <hideyoshi.playing.the.banjo@gmail.com>
2021-02-18 23:00:55 +0900

    トップ画面にデザイン反映。リファクタリング。

--- homeScreen.js
+++ homeScreen.js
@@ -1,80 +1,108 @@
 import React from 'react';
-import { SafeAreaView, View, StyleSheet, Text, Button } from 'react-native';
+import { SafeAreaView, View, StyleSheet, Text, Button, ImageBackground } from 'react-native';
 import { TouchableOpacity } from 'react-native-gesture-handler';
+
+const TitleLogo = () => (
+  <View style={titleStyle.wrapper}>
+    <View>
+      <Text style={textTitleMain}>F.I.R.E</Text>
+    </View>
+    <View>
+      <Text style={textTitleSub}>Countdown App</Text>
+    </View>
+  </View>
+)
+
+const Header = () => {
+  return(
+    <View><TitleLogo/></View>
+  )
+};
+
+const restYears = (years) => (years ? years : '??')
+
+const restMonths = (months) => (months ? months :'?')
+
+function NoticeNoConfig(props) {
+  if (props.unset_configs.length) {
+    return (
+      <View style={unsetConfigStyle.wrapper}>
+        <View style={unsetConfigStyle.text}><Text style={unsetConfigStyle.text}>未設定の項目があります</Text></View>
+        <View><Text style={unsetConfigStyle.text}>({props.unset_configs.join("、 ")})</Text></View>
+      </View>
+    )
+  } else {
+    return (
+      <View style={unsetConfigStyle.wrapper}></View>
+    )
+  }
+}
+
+function RestYear(props) {
+  return (
+    <View style={contentStyle.numberBlock}>
+      <Text style={contentStyle.countdownNumber}>{restYears(props.restYears)}</Text>
+    </View>
+  )
+}
+
+function RestMonth(props) {
+  return (
+    <View style={contentStyle.numberBlock}>
+      <Text style={contentStyle.countdownNumber}>{restMonths(props.restMonths)}</Text>
+    </View>
+  )
+}
+
+function Unit(props) {
+  return (
+    <View style={contentStyle.unitBlock}>
+      <Text style={contentStyle.countdownUnit}>{props.unitName}</Text>
+    </View>
+  )
+}
+
+// TODO: リファクタリング
+function Content(props) {
+  return (
+    <View style={contentStyle.wrapper}>
+      <View><Text style={contentStyle.countdownMessage}>リタイアまで</Text></View>
+      <View style={contentStyle.countdownDate}>
+        <RestYear restYears={props.rest_years}/>
+        <Unit unitName='年'/>
+        <RestMonth restMonths={props.rest_months}/>
+        <Unit unitName='ヶ月'/>
+      </View>
+      <NoticeNoConfig unset_configs={props.unset_configs} />
+    </View>
+  )
+};
 
 function Item(props) {
   return (
-    <TouchableOpacity style={styles.item} onPress={() => {props.onPress()}}>
-      <Text style={styles.title}>{props.title}</Text>
+    <TouchableOpacity style={configStyle.item} onPress={() => {props.onPress()}}>
+      <Text style={configStyle.title}>{props.title}</Text>
     </TouchableOpacity>
   )
 };
 
-// TODO: リファクタリング
-function Content(props) {
-  if (props.jwtToken) {
-    if (props.rest_years) {
-      return (
-        <View style={styles.content}>
-          <Text style={styles.mainMessage}>{props.rest_years}年 {props.rest_months}ヶ月後にリタイア</Text>
-        </View>
-      )
-    } else if (props.unset_configs) {
-      const array = props.unset_configs
-      return (
-        <View>
-          <Text>{props.unset_configs.join("、 ")}が未設定です。</Text>
-        </View>
-      )
-    } else {
-      return(
-        <View>
-          <Text></Text>
-        </View>
-      )
-    }
-  } else {
-    return (
-      <View>
-        <Text>ようこそ！まずはユーザー登録から始めましょう！！</Text>
-      </View>
-    )
-  }
-};
-
-
-const UserName = ({username}) => (
-  <Text style={styles.userNameText}>{username}さん、こんにちは！</Text>
-);
-
-function Header(props) {
+function ItemList(props) {
   return (
     <View>
-      <UserName username={props.username}></UserName>
-      <View style={styles.headerMenu}>
-        <Button
-          title ="ログアウト"
-          onPress={() => {props.onSignout()}}
-        />
-      </View>
+      <Item title='メイン設定' onPress={() => {props.onPressConfig()}} />
+      <Item title='リタイア額計算' onPress={() => {props.onPressRetirementAssetConfig()}}/>
     </View>
-  );
-};
+  )
+}
 
-function ItemList(props) {
-  if (props.jwtToken) {
-    return (
-      <View>
-        <Item title='メイン設定' onPress={() => {props.onPressConfig()}} />
-        <Item title='リタイア額計算' onPress={() => {props.onPressRetirementAssetConfig()}}/>
-      </View>
-    )
-  } else {
-    return (
-      <View>
-      </View>
-    )
-  }
+function Footer(props) {
+  return (
+    <View style={footerStyle.wrapper}>
+      <TouchableOpacity onPress={() => {props.onSignout()}}>
+        <Text style={footerStyle.text}>サインアウト</Text>
+      </TouchableOpacity>
+    </View>
+  )
 }
 
 class HomeScreen extends React.Component {
@@ -84,7 +112,7 @@ class HomeScreen extends React.Component {
       username: null,
       rest_years: null,
       rest_months: null,
-      unset_config: null,
+      unset_configs: [],
       mounted: true
     };
   }
@@ -142,73 +170,137 @@ class HomeScreen extends React.Component {
     });
   }
 
-
-
   render() {
     return (
       <SafeAreaView style={styles.screen}>
-        <Header
-          onSignout={() => {this.props.onSignout()}}
-          username={this.state.username}
-        />
-        <Content
-          jwtToken={this.props.jwtToken}
-          {...this.state}
-        />
+        <ImageBackground source={backgroundImage} style={styles.backgroundImage}/>
+        <Header/>
+        <Content {...this.state}/>
         <ItemList
           onPressConfig={() => {this.props.navigation.navigate('Config')}}
           onPressRetirementAssetConfig={() => {this.props.navigation.navigate('RetirementAssetConfig')}}
-          jwtToken={this.props.jwtToken}
         />
+        <Footer onSignout={() => {this.props.onSignout()}}/>
       </SafeAreaView>
     );
   }
 }
 
+const backgroundImage = require('./assets/background_img.jpg')
+
 const styles = StyleSheet.create({
   screen: {
     ...StyleSheet.absoluteFillObject,
+    padding: 40,
+    alignItems: 'center'
   },
-  headerMenu: {
-    flexDirection: 'row',
+  backgroundImage: {
+    ...StyleSheet.absoluteFillObject,
+    top: -270,
+    left: -340,
+    width: 1100
   },
-  headerMenuItem: {
-    flex: 1,
-    flexDirection: 'row',
-    marginVertical: 4,
-    marginHorizontal: 12
+});
+
+const titleStyle = StyleSheet.create({
+  wrapper: {
+    marginTop: 80,
+    marginBottom: 50,
+    width: 310,
+    alignItems: 'flex-end'
   },
-  userName: {
-    marginHorizontal: 10
+  textCommon: {
+    color: '#ffffff'
   },
-  userNameText: {
+  textMain: {
+    fontSize: 53,
+    textAlign: 'center'
+  },
+  textSub: {
+    fontSize: 19,
+    textAlign: 'center'
+  }
+})
+
+const textTitleMain = StyleSheet.compose(titleStyle.textMain, titleStyle.textCommon)
+const textTitleSub = StyleSheet.compose(titleStyle.textSub, titleStyle.textCommon)
+
+const contentStyle = StyleSheet.create({
+  wrapper: {
+    width: 310,
+    marginBottom: 36,
+  },
+  countdownMessage: {
+    marginBottom: 6,
+    fontSize: 24,
     fontWeight: 'bold',
-    fontSize: 20
+    color: '#ffffff'
   },
+  countdownDate: {
+    marginBottom: 6,
+    alignItems: 'flex-end',
+    flexDirection: 'row',
+  },
+  numberBlock: {
+    marginRight: 6
+  },
+  unitBlock:{
+    marginRight: 18,
+    position: 'relative',
+    bottom: 15
+  },
+  countdownNumber: {
+    fontSize: 64,
+    fontWeight: 'bold',
+    color: '#ffffff'
+  },
+  countdownUnit: {
+    fontSize: 36,
+    fontWeight: 'bold',
+    color: '#ffffff'
+  },
+})
+
+const unsetConfigStyle = StyleSheet.create({
+  wrapper: {
+    height: 30
+  },
+  text: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: '#ffffff'
+  }
+})
+
+const configStyle = StyleSheet.create({
   item: {
-    backgroundColor: '#f9c2ff',
-    padding: 20,
-    marginVertical: 8,
-    marginHorizontal: 16,
+    backgroundColor: '#EDB413',
+    marginBottom: 32,
     borderRadius: 10,
+    height: 48,
+    width: 310,
+    justifyContent: 'center',
+    alignItems: 'center'
   },
   title: {
     color: '#ffffff',
     fontSize: 32,
-  },
-  content: {
-    padding: 20,
-    marginVertical: 8,
-    marginHorizontal: 16,
-  },
-  mainMessage: {
-    fontSize: 42
-  },
-  basicFlex: {
-    flex: 1,
-    flexDirection: 'row',
-    justifyContent: 'space-between'
+    fontWeight: '100'
   }
-});
+})
+
+const footerStyle = StyleSheet.create({
+  wrapper: {
+    width: 310,
+    position: 'absolute',
+    bottom:40,
+    alignItems: 'flex-end'
+  },
+  text: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: '#ffffff'
+  }
+})
 
 export default HomeScreen;
